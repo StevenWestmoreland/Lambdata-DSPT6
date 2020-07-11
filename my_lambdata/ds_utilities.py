cf5dbad6c58486e9f9e499eb72c378028f4338b3
StevenWestmoreland <stevenmwestmoreland@gmail.com>
2020-07-11 13:34:56 -0400

    Sanity Check 2

--- my_lambdata/ds_utilities.py
+++ my_lambdata/ds_utilities.py
@@ -1,30 +1,45 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-def enlarge(n): 
-    ''' This function will multiply the input by 100 '''
+
+def enlarge(n):
+    '''This function will multiply the input by 100'''
     return n * 100
 
-if __name__ == '__main__':
-    y = int(input("Choose a number: "))
-    print(y, enlarge(y))
-    
-def split(X, splitn, valn):
-    ''' This function will take dataframe X and perform a train/test split by the
-    value of splitn. It will then split the train into train and validation by the 
-    value of valn. Both splitn and valn must be between 0 and 1. '''
-    
-    train_no_val, test = train_test_split(X, train_size=splitn, test_size=1-splitn, random_state=42)
-    train, val = train_test_split(train_no_val, train_size=valn, test_size=1-valn, random_state=42)
 
-    return train.shape, val.shape, test.shape
+def train_val_test_split(X, y, train_size=0.7, val_size=0.1,
+                        test_size=0.2, random_state=None, shuffle=True):
+    '''Split a dataframe into train, validation, and test dataframes.
 
-def datesplit(X, col, drop):
-    ''' For dataframe X, input a date feature col to create three new features for
-    the dataframe: day, month, and year. Also allows for dropping of original feature '''
+    Keyword arguments:
+    X -- dataframe to be split
+    y -- target feature
+    train_size -- proportion of dataset to include in test (default 0.7)
+    val_size -- proportion of dataset to include in validation (default 0.1)
+    test_size -- proportion of dataset to include in test (default 0.2)
+    random_state -- pass int for reproducibility across multiple calls (default None)
+    shuffle -- whether or not to shuffle data pre-split (default True)
+    '''
+    X_train_val, X_test, y_train_val, y_test = train_test_split(
+        X, y, test_size=test_size, random_state=random_state, shuffle=shuffle)
+    
+    X_train, X_val, y_train, y_val = train_test_split(
+        X_train_val, y_train_val, test_size=val_size / (train_size + val_size),
+        random_state=random_state, shuffle=shuffle)
+
+    return X_train, X_val, X_test, y_train, y_val, y_test
+
+
+def datesplit(X, col):
+    '''Split a time based feature into day, month, and year features.
+
+    Keyword arguments:
+    X -- dataframe containing the time feature
+    col -- time based feature
+    '''
 
     X['year'] = pd.to_datetime(X[col]).dt.year
     X['month'] = pd.to_datetime(X[col]).dt.month
     X['day'] = pd.to_datetime(X[col]).dt.day
-
+    
     return X
